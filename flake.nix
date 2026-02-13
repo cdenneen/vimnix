448bbc6642003e015ab5bc5dd258836c365b2f9f
Chris Denneen <cdenneen@gmail.com>
2026-02-12 18:42:24 -0600

    Cleanup

--- flake.nix
+++ flake.nix
@@ -18,108 +18,121 @@
       url = "github:kamadorueda/alejandra/4.0.0";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
-    modeling-app = {
-      url = "github:kittycad/modeling-app";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    home-manager,
-    rust-overlay,
-    alejandra,
-    modeling-app,
-    ...
-  }: let
-    supportedSystems = ["x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"];
-
-    overlays = [rust-overlay.overlays.default];
-
-    mkPkgs = system:
-      import nixpkgs {
-        inherit system overlays;
-      };
-
-    forAllSystems = f:
-      nixpkgs.lib.genAttrs supportedSystems (system:
-        f {
-          pkgs = mkPkgs system;
-          system = system;
-        });
-  in {
-    homeManagerModules.default = {
-      pkgs,
-      config,
-      lib,
+  outputs =
+    {
+      self,
+      nixpkgs,
+      home-manager,
+      rust-overlay,
+      alejandra,
       ...
-    }: let
-      mkIfExists = path:
-        if builtins.pathExists path
-        then path
-        else pkgs.emptyFile;
-
-      alejandraPkg = alejandra.packages.${pkgs.system}.default;
-      # Temporary fix for apple sdks
-      rustAnalyzer =
-        if pkgs.stdenv.isDarwin
-        then pkgs.rust-analyzer
-        else pkgs.rust-analyzer-nightly;
-      kclLsp = modeling-app.packages.${pkgs.system}.kcl-language-server;
-    in {
-      home.packages = with pkgs; [
-        alejandraPkg
-        biome
-        clang-tools
-        gh
-        go
-        gofumpt
-        gotools
-        gopls
-        jq
-        kclLsp
-        luajitPackages.jsregexp
-        luajitPackages.luarocks
-        mdformat
-        nixd
-        pyright
-        ripgrep
-        rustAnalyzer
-        ruff
-        stylua
-        taplo
-        tree-sitter
-        typescript
-        typescript-language-server
-        yamlfmt
+    }:
+    let
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
       ];
 
-      programs.neovim = {
-        enable = true;
-        defaultEditor = true;
-        viAlias = true;
-        vimAlias = true;
+      overlays = [ rust-overlay.overlays.default ];
 
-        package = pkgs.neovim-unwrapped;
-      };
-
-      home.file = {
-        ".config/nvim/init.lua".source = ./init.lua;
-
-        ".config/nvim/lua" = {
-          source = mkIfExists ./lua;
+      mkPkgs =
+        system:
+        import nixpkgs {
+          inherit system overlays;
         };
-      };
-    };
 
-    homeConfigurations = forAllSystems (
-      {system, ...}:
+      forAllSystems =
+        f:
+        nixpkgs.lib.genAttrs supportedSystems (
+          system:
+          f {
+            pkgs = mkPkgs system;
+            system = system;
+          }
+        );
+    in
+    {
+      homeManagerModules.default =
+        {
+          pkgs,
+          config,
+          lib,
+          ...
+        }:
+        let
+          mkIfExists = path: if builtins.pathExists path then path else pkgs.emptyFile;
+
+          alejandraPkg = alejandra.packages.${pkgs.stdenv.hostPlatform.system}.default;
+          # Temporary fix for apple sdks
+          rustAnalyzer = pkgs.rust-analyzer;
+        in
+        {
+          home.packages = with pkgs; [
+            alejandraPkg
+            biome
+            clang-tools
+            gh
+            go
+            gofumpt
+            gotools
+            gopls
+            jq
+            luajitPackages.jsregexp
+            luajitPackages.luarocks
+            mdformat
+            nixd
+            pyright
+            ripgrep
+            rustAnalyzer
+            ruff
+            stylua
+            taplo
+            tree-sitter
+            typescript
+            typescript-language-server
+            yamlfmt
+          ];
+
+          programs.neovim = {
+            enable = true;
+            defaultEditor = true;
+            viAlias = true;
+            vimAlias = true;
+
+            package = pkgs.neovim-unwrapped;
+          };
+
+          home.file = {
+            ".config/nvim/init.lua".source = ./init.lua;
+
+            ".config/nvim/lua" = {
+              source = mkIfExists ./lua;
+            };
+          };
+        };
+
+      homeConfigurations = forAllSystems (
+        { system, ... }:
         home-manager.lib.homeManagerConfiguration {
           pkgs = mkPkgs system;
-          modules = [self.homeManagerModules.default];
+          modules = [
+            self.homeManagerModules.default
+            (
+              { pkgs, ... }:
+              {
+                home = {
+                  stateVersion = "25.11";
+                  username = "nobody";
+                  homeDirectory = if pkgs.stdenv.isDarwin then "/Users/nobody" else "/home/nobody";
+                };
+              }
+            )
+          ];
         }
-    );
-  };
+      );
+    };
 }
